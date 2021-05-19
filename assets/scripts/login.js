c13f82b54c15971f4d9ee1e47f41c82032fe7f52
Luiz Dorval <luizdorval.silva@gmail.com>
2021-05-19 20:10:37 -0300

    fixes in login and styles

--- assets/scripts/login.js
+++ assets/scripts/login.js
@@ -1,7 +1,17 @@
+window.onload = () => {
+    if(localStorage.getItem('4elementsUsernamevfx') && localStorage.getItem('4elementsPasswordvfx')) {
+    document.querySelector('#name').value = localStorage.getItem('4elementsUsernamevfx')
+    document.querySelector('#passwd').value = localStorage.getItem('4elementsPasswordvfx')
+    login();
+    }
+}
+
 const login = () => {
-    console.log('funfou')
     if(document.querySelector('#name').value == "admin" && document.querySelector('#passwd').value == "12qwaszx") {
-        document.querySelector('#loginSect').style.display = 'none'
+        localStorage.setItem('4elementsUsernamevfx', 'admin')
+        localStorage.setItem('4elementsPasswordvfx', '12qwaszx')
+        document.querySelector('#loginSect').style.height = '0vh'
+        document.querySelector('#loginSect').innerHTML = '<h1 style="font-size: 2rem">Bem-Vindo!<h1>'
         document.querySelector('main').style.display = 'flex'
     } else {
         document.querySelector('#loginSect').style.display = 'flex'
